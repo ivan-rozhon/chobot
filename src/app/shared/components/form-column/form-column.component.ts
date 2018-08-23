5f59e5238c11d32b28626089f0952ea440e4a2c3
Rozhoň Ivan <irozhon@trask.cz>
2018-08-23 22:16:45 +0200

    half-split form-column; toggleable form-title

--- src/app/shared/components/form-column/form-column.component.ts
+++ src/app/shared/components/form-column/form-column.component.ts
@@ -1,5 +1,5 @@
 import { Component, HostBinding, Input, OnInit, ViewEncapsulation } from '@angular/core';
-import { coerceNumberProperty } from '@angular/cdk/coercion';
+import { coerceNumberProperty, coerceBooleanProperty } from '@angular/cdk/coercion';
 
 import { FormBlockComponent } from '@shared/components/form-block/form-block.component';
 
@@ -11,6 +11,8 @@ import { FormBlockComponent } from '@shared/components/form-block/form-block.com
 })
 export class FormColumnComponent implements OnInit {
   private _columns: number;
+  private basicColumnWidth = 180;
+  private basicColumnPadding = 16;
 
   @Input('columnsPart')
   get columns() {
@@ -18,13 +20,28 @@ export class FormColumnComponent implements OnInit {
   }
   set columns(value: any) {
     // transform input value to number
-    this._columns = coerceNumberProperty(value);
+    const columns = coerceNumberProperty(value);
+
+    // set number of columns
+    this._columns = columns;
+
+    // calculate min width of column according to number of columns
+    this.minWidthStyle = `${this.basicColumnWidth * columns + 2 * this.basicColumnPadding}px`;
+  }
+  @Input()
+  set halfSplit(value: any) {
+    // transform input value to boolean
+    this.halfSplitClass = coerceBooleanProperty(value);
   }
 
   @HostBinding('style.width')
   widthStyle: string;
+  @HostBinding('style.minWidth')
+  minWidthStyle: string;
   @HostBinding('class.cc-form-column')
   formColumnClass = true;
+  @HostBinding('class.half-split')
+  halfSplitClass;
 
   constructor(private formBlockRef: FormBlockComponent) {}
 
--- src/app/shared/components/form-title/form-title.component.ts
+++ src/app/shared/components/form-title/form-title.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
 
 @Component({
   selector: 'cc-form-title',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-title.component.scss']
 })
 export class FormTitleComponent {
+  private _toggleable: boolean;
+  @Input()
+  get toggleable() {
+    return this._toggleable;
+  }
+  set toggleable(value: any) {
+    // transform input value to boolean
+    this._toggleable = coerceBooleanProperty(value);
+  }
+
   constructor() {}
 }
